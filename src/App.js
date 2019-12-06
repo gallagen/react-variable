cd51cd94abc392aaad9b94ceccd42193d21ae8ea
Galina <galina@Olegs-MacBook-Pro.local>
2019-12-06 16:55:54 +0300

    Style color

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ function App() {
   const date = new Date();
   const hours = date.getHours();
   let timeOfDay;
+  const styles = {
+    fontSize: 30
+  };
 
   if (hours < 12) {
     timeOfDay = "morning"
+    styles.color = "#04756F"
   } else if (hours >= 12 && hours <= 17) {
     timeOfDay = "afternoon"
+    styles.color = "#E11111"
   } else {
     timeOfDay = "night"
+    styles.color = "#D90000"
   }
 
-  return <h1>Good {timeOfDay}!</h1>;
+  return (
+    <h1 style={styles}>Good {timeOfDay}!</h1>
+  )
 }
 
 export default App;
